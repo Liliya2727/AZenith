04b81672070182b0b2add5da7758e94a519c38f7
Liliya2727 <syarifbintang16@gmail.com>
2026-02-23 20:41:34 +0700

    fixed a bug where PropertyUtils, doesnt set ColorScheme Values

--- manager/app/src/main/java/zx/azenith/ui/util/SystemProperties.kt
+++ manager/app/src/main/java/zx/azenith/ui/util/SystemProperties.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2026-2027 Zexshia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package zx.azenith.ui.util
 
 import android.annotation.SuppressLint
@@ -22,8 +38,7 @@ object PropertyUtils {
             val set = systemProperties.getMethod("set", String::class.java, String::class.java)
             set.invoke(null, key, value)
         } catch (e: Exception) {
-            // Gunakan Shell jika reflection gagal (karena permission)
-            Shell.cmd("setprop $key $value").submit()
+            Shell.cmd("setprop $key '$value'").submit()
         }
     }
 }
