06a2a484d5d933dc3a539a726663b90f4b790447
Liliya2727 <syarifbintang16@gmail.com>
2026-06-02 03:34:37 +0700

    added I/O Tweaks on Initializing and all Profiles

--- binprofiles/src/profiles/mod.rs
+++ binprofiles/src/profiles/mod.rs
@@ -52,41 +52,41 @@ pub fn performance_profile() {
 
     let pl_base = "/sys/devices/system/cpu/perf";
     if Path::new(pl_base).exists() {
-        zeshia("1", &format!("{}/gpu_pmu_enable", pl_base));
-        zeshia("1", &format!("{}/fuel_gauge_enable", pl_base));
-        zeshia("1", &format!("{}/enable", pl_base));
-        zeshia("1", &format!("{}/charger_enable", pl_base));
+        zeshia_def("1", &format!("{}/gpu_pmu_enable", pl_base));
+        zeshia_def("1", &format!("{}/fuel_gauge_enable", pl_base));
+        zeshia_def("1", &format!("{}/enable", pl_base));
+        zeshia_def("1", &format!("{}/charger_enable", pl_base));
     }
 
-    zeshia("40", "/proc/sys/vm/vfs_cache_pressure");
-    zeshia("3", "/proc/sys/vm/drop_caches");
-    zeshia("N", "/sys/module/workqueue/parameters/power_efficient");
-    zeshia("N", "/sys/module/workqueue/parameters/disable_numa");
-    zeshia("0", "/sys/kernel/eara_thermal/enable");
-    zeshia("0", "/sys/devices/system/cpu/eas/enable");
-    zeshia("1", "/sys/devices/system/cpu/cpu2/online");
-    zeshia("1", "/sys/devices/system/cpu/cpu3/online");
+    zeshia_def("80", "/proc/sys/vm/vfs_cache_pressure");
+    zeshia_def("3", "/proc/sys/vm/drop_caches");
+    zeshia_def("N", "/sys/module/workqueue/parameters/power_efficient");
+    zeshia_def("N", "/sys/module/workqueue/parameters/disable_numa");
+    zeshia_def("0", "/sys/kernel/eara_thermal/enable");
+    zeshia_def("0", "/sys/devices/system/cpu/eas/enable");
+    zeshia_def("1", "/sys/devices/system/cpu/cpu2/online");
+    zeshia_def("1", "/sys/devices/system/cpu/cpu3/online");
 
     if let Ok(paths) = glob::glob("/dev/stune/*") {
         for path in paths.flatten() {
             if path.is_dir() {
                 let p_str = path.to_str().unwrap();
-                zeshia("30", &format!("{}/schedtune.boost", p_str));
-                zeshia("1", &format!("{}/schedtune.sched_boost_enabled", p_str));
-                zeshia("0", &format!("{}/schedtune.prefer_idle", p_str));
-                zeshia("0", &format!("{}/schedtune.colocate", p_str));
+                zeshia_def("30", &format!("{}/schedtune.boost", p_str));
+                zeshia_def("1", &format!("{}/schedtune.sched_boost_enabled", p_str));
+                zeshia_def("0", &format!("{}/schedtune.prefer_idle", p_str));
+                zeshia_def("0", &format!("{}/schedtune.colocate", p_str));
             }
         }
     }
 
-    zeshia("1", "/proc/sys/kernel/perf_cpu_time_max_percent");
-    zeshia("1", "/proc/sys/kernel/sched_energy_aware");
+    zeshia_def("1", "/proc/sys/kernel/perf_cpu_time_max_percent");
+    zeshia_def("1", "/proc/sys/kernel/sched_energy_aware");
 
     if let Ok(paths) = glob::glob("/sys/devices/system/cpu/cpu*") {
         for path in paths.flatten() {
             let p_str = path.to_str().unwrap();
-            zeshia("0", &format!("{}/core_ctl/enable", p_str));
-            zeshia("0", &format!("{}/core_ctl/core_ctl_boost", p_str));
+            zeshia_def("0", &format!("{}/core_ctl/enable", p_str));
+            zeshia_def("0", &format!("{}/core_ctl/core_ctl_boost", p_str));
         }
     }
 
@@ -94,19 +94,40 @@ pub fn performance_profile() {
     if Path::new(bs_path).exists() {
         let content = fs::read_to_string(bs_path).unwrap_or_default();
         if content.chars().any(|c: char| c.is_ascii_digit()) {
-            zeshia("0", bs_path);
+            zeshia_def("0", bs_path);
         } else {
-            zeshia("N", bs_path);
+            zeshia_def("N", bs_path);
         }
     }
 
-    zeshia("0", "/proc/sys/kernel/split_lock_mitigate");
+    zeshia_def("0", "/proc/sys/kernel/split_lock_mitigate");
 
     let sched_feat = "/sys/kernel/debug/sched_features";
     if Path::new(sched_feat).exists() {
-        zeshia("NEXT_BUDDY", sched_feat);
-        zeshia("NO_TTWU_QUEUE", sched_feat);
+        zeshia_def("NEXT_BUDDY", sched_feat);
+        zeshia_def("NO_TTWU_QUEUE", sched_feat);
     }
+    
+    // I/O Tweaks
+    std::thread::spawn(|| {
+        if let Ok(paths) = glob::glob("/sys/block/*") {
+            for path in paths.flatten() {
+                // Ambil nama folder/device (misal: "mmcblk0", "sda")
+                if let Some(file_name) = path.file_name().and_then(|n| n.to_str()) {
+                    // Filter sesuai spesifikasi: mmcblk0, mmcblk1, atau yang berawalan "sd"
+                    if file_name == "mmcblk0" || file_name == "mmcblk1" || file_name.starts_with("sd") {
+                        if let Some(p_str) = path.to_str() {
+                            // Reduce heuristic read-ahead in exchange for I/O latency
+                            zeshia_def("32", &format!("{}/queue/read_ahead_kb", p_str));
+    
+                            // Reduce the maximum number of I/O requests in exchange for latency
+                            zeshia_def("32", &format!("{}/queue/nr_requests", p_str));
+                        }
+                    }
+                }
+            }
+        }
+    });
 
     if get_clearapps() {
         clear_background_apps();
@@ -161,39 +182,39 @@ pub fn balanced_profile() {
 
     let pl_base = "/sys/devices/system/cpu/perf";
     if Path::new(pl_base).exists() {
-        zeshia("0", &format!("{}/gpu_pmu_enable", pl_base));
-        zeshia("0", &format!("{}/fuel_gauge_enable", pl_base));
-        zeshia("0", &format!("{}/enable", pl_base));
-        zeshia("1", &format!("{}/charger_enable", pl_base));
+        zeshia_def("0", &format!("{}/gpu_pmu_enable", pl_base));
+        zeshia_def("0", &format!("{}/fuel_gauge_enable", pl_base));
+        zeshia_def("0", &format!("{}/enable", pl_base));
+        zeshia_def("1", &format!("{}/charger_enable", pl_base));
     }
 
-    zeshia("120", "/proc/sys/vm/vfs_cache_pressure");
-    zeshia("Y", "/sys/module/workqueue/parameters/power_efficient");
-    zeshia("Y", "/sys/module/workqueue/parameters/disable_numa");
-    zeshia("1", "/sys/kernel/eara_thermal/enable");
-    zeshia("1", "/sys/devices/system/cpu/eas/enable");
+    zeshia_def("120", "/proc/sys/vm/vfs_cache_pressure");
+    zeshia_def("Y", "/sys/module/workqueue/parameters/power_efficient");
+    zeshia_def("Y", "/sys/module/workqueue/parameters/disable_numa");
+    zeshia_def("1", "/sys/kernel/eara_thermal/enable");
+    zeshia_def("1", "/sys/devices/system/cpu/eas/enable");
 
     if let Ok(paths) = glob::glob("/dev/stune/*") {
         for path in paths.flatten() {
             if path.is_dir() {
                 let p_str = path.to_str().unwrap();
-                zeshia("0", &format!("{}/schedtune.boost", p_str));
-                zeshia("0", &format!("{}/schedtune.sched_boost_enabled", p_str));
-                zeshia("0", &format!("{}/schedtune.prefer_idle", p_str));
-                zeshia("0", &format!("{}/schedtune.colocate", p_str));
+                zeshia_def("0", &format!("{}/schedtune.boost", p_str));
+                zeshia_def("0", &format!("{}/schedtune.sched_boost_enabled", p_str));
+                zeshia_def("0", &format!("{}/schedtune.prefer_idle", p_str));
+                zeshia_def("0", &format!("{}/schedtune.colocate", p_str));
             }
         }
     }
 
-    zeshia("100", "/proc/sys/kernel/perf_cpu_time_max_percent");
-    zeshia("2", "/proc/sys/kernel/perf_cpu_time_max_percent");
-    zeshia("1", "/proc/sys/kernel/sched_energy_aware");
+    zeshia_def("100", "/proc/sys/kernel/perf_cpu_time_max_percent");
+    zeshia_def("2", "/proc/sys/kernel/perf_cpu_time_max_percent");
+    zeshia_def("1", "/proc/sys/kernel/sched_energy_aware");
 
     if let Ok(paths) = glob::glob("/sys/devices/system/cpu/cpu*") {
         for path in paths.flatten() {
             let p_str = path.to_str().unwrap();
-            zeshia("0", &format!("{}/core_ctl/enable", p_str));
-            zeshia("0", &format!("{}/core_ctl/core_ctl_boost", p_str));
+            zeshia_def("0", &format!("{}/core_ctl/enable", p_str));
+            zeshia_def("0", &format!("{}/core_ctl/core_ctl_boost", p_str));
         }
     }
 
@@ -201,19 +222,40 @@ pub fn balanced_profile() {
     if Path::new(bs_path).exists() {
         let content = fs::read_to_string(bs_path).unwrap_or_default();
         if content.chars().any(|c: char| c.is_ascii_digit()) {
-            zeshia("0", bs_path);
+            zeshia_def("0", bs_path);
         } else {
-            zeshia("N", bs_path);
+            zeshia_def("N", bs_path);
         }
     }
 
-    zeshia("1", "/proc/sys/kernel/split_lock_mitigate");
+    zeshia_def("1", "/proc/sys/kernel/split_lock_mitigate");
 
     let sched_feat = "/sys/kernel/debug/sched_features";
     if Path::new(sched_feat).exists() {
-        zeshia("NEXT_BUDDY", sched_feat);
-        zeshia("TTWU_QUEUE", sched_feat);
+        zeshia_def("NEXT_BUDDY", sched_feat);
+        zeshia_def("TTWU_QUEUE", sched_feat);
     }
+    
+    // I/O Tweaks
+    std::thread::spawn(|| {
+        if let Ok(paths) = glob::glob("/sys/block/*") {
+            for path in paths.flatten() {
+                // Ambil nama folder/device (misal: "mmcblk0", "sda")
+                if let Some(file_name) = path.file_name().and_then(|n| n.to_str()) {
+                    // Filter sesuai spesifikasi: mmcblk0, mmcblk1, atau yang berawalan "sd"
+                    if file_name == "mmcblk0" || file_name == "mmcblk1" || file_name.starts_with("sd") {
+                        if let Some(p_str) = path.to_str() {
+                            // Reduce heuristic read-ahead in exchange for I/O latency
+                            zeshia_def("128", &format!("{}/queue/read_ahead_kb", p_str));
+    
+                            // Reduce the maximum number of I/O requests in exchange for latency
+                            zeshia_def("64", &format!("{}/queue/nr_requests", p_str));
+                        }
+                    }
+                }
+            }
+        }
+    });
 
     match getprop("persist.sys.azenithdebug.soctype").as_str() {
         "1" => mediatek_balance(),
@@ -251,39 +293,39 @@ pub fn eco_mode() {
 
     let pl_base = "/sys/devices/system/cpu/perf";
     if Path::new(pl_base).exists() {
-        zeshia("0", &format!("{}/gpu_pmu_enable", pl_base));
-        zeshia("0", &format!("{}/fuel_gauge_enable", pl_base));
-        zeshia("0", &format!("{}/enable", pl_base));
-        zeshia("1", &format!("{}/charger_enable", pl_base));
+        zeshia_def("0", &format!("{}/gpu_pmu_enable", pl_base));
+        zeshia_def("0", &format!("{}/fuel_gauge_enable", pl_base));
+        zeshia_def("0", &format!("{}/enable", pl_base));
+        zeshia_def("1", &format!("{}/charger_enable", pl_base));
     }
 
-    zeshia("120", "/proc/sys/vm/vfs_cache_pressure");
-    zeshia("Y", "/sys/module/workqueue/parameters/power_efficient");
-    zeshia("Y", "/sys/module/workqueue/parameters/disable_numa");
-    zeshia("1", "/sys/kernel/eara_thermal/enable");
-    zeshia("1", "/sys/devices/system/cpu/eas/enable");
+    zeshia_def("120", "/proc/sys/vm/vfs_cache_pressure");
+    zeshia_def("Y", "/sys/module/workqueue/parameters/power_efficient");
+    zeshia_def("Y", "/sys/module/workqueue/parameters/disable_numa");
+    zeshia_def("1", "/sys/kernel/eara_thermal/enable");
+    zeshia_def("1", "/sys/devices/system/cpu/eas/enable");
 
     if let Ok(paths) = glob::glob("/dev/stune/*") {
         for path in paths.flatten() {
             if path.is_dir() {
                 let p_str = path.to_str().unwrap();
-                zeshia("0", &format!("{}/schedtune.boost", p_str));
-                zeshia("0", &format!("{}/schedtune.sched_boost_enabled", p_str));
-                zeshia("0", &format!("{}/schedtune.prefer_idle", p_str));
-                zeshia("0", &format!("{}/schedtune.colocate", p_str));
+                zeshia_def("0", &format!("{}/schedtune.boost", p_str));
+                zeshia_def("0", &format!("{}/schedtune.sched_boost_enabled", p_str));
+                zeshia_def("0", &format!("{}/schedtune.prefer_idle", p_str));
+                zeshia_def("0", &format!("{}/schedtune.colocate", p_str));
             }
         }
     }
 
-    zeshia("50", "/proc/sys/kernel/perf_cpu_time_max_percent");
-    zeshia("0", "/proc/sys/kernel/perf_cpu_time_max_percent");
-    zeshia("0", "/proc/sys/kernel/sched_energy_aware");
+    zeshia_def("50", "/proc/sys/kernel/perf_cpu_time_max_percent");
+    zeshia_def("0", "/proc/sys/kernel/perf_cpu_time_max_percent");
+    zeshia_def("0", "/proc/sys/kernel/sched_energy_aware");
 
     if let Ok(paths) = glob::glob("/sys/devices/system/cpu/cpu*") {
         for path in paths.flatten() {
             let p_str = path.to_str().unwrap();
-            zeshia("0", &format!("{}/core_ctl/enable", p_str));
-            zeshia("0", &format!("{}/core_ctl/core_ctl_boost", p_str));
+            zeshia_def("0", &format!("{}/core_ctl/enable", p_str));
+            zeshia_def("0", &format!("{}/core_ctl/core_ctl_boost", p_str));
         }
     }
 
@@ -291,18 +333,31 @@ pub fn eco_mode() {
     if Path::new(bs_path).exists() {
         let content = fs::read_to_string(bs_path).unwrap_or_default();
         if content.chars().any(|c: char| c.is_ascii_digit()) {
-            zeshia("1", bs_path);
+            zeshia_def("1", bs_path);
         } else {
-            zeshia("Y", bs_path);
+            zeshia_def("Y", bs_path);
         }
     }
 
-    zeshia("1", "/proc/sys/kernel/split_lock_mitigate");
+    zeshia_def("1", "/proc/sys/kernel/split_lock_mitigate");
 
     let sched_feat = "/sys/kernel/debug/sched_features";
     if Path::new(sched_feat).exists() {
-        zeshia("NO_NEXT_BUDDY", sched_feat);
-        zeshia("NO_TTWU_QUEUE", sched_feat);
+        zeshia_def("NO_NEXT_BUDDY", sched_feat);
+        zeshia_def("NO_TTWU_QUEUE", sched_feat);
+    }
+    
+    // Enable battery saver module
+    let bs_path = "/sys/module/battery_saver/parameters/enabled";
+    if Path::new(bs_path).exists() {
+        let content = fs::read_to_string(bs_path).unwrap_or_default();
+        
+        // Mengecek apakah ada angka (mirip dengan grep -qo '[0-9]\+')
+        if content.chars().any(|c| c.is_ascii_digit()) {
+            zeshia_def("1", bs_path);
+        } else {
+            zeshia_def("Y", bs_path);
+        }
     }
 
     match getprop("persist.sys.azenithdebug.soctype").as_str() {
@@ -319,7 +374,7 @@ pub fn eco_mode() {
 
 pub fn initialize() {
     for param in &["panic", "panic_on_warn", "panic_on_oops", "softlockup_panic"] {
-        zeshia("0", &format!("/proc/sys/kernel/{}", param));
+        zeshia_def("0", &format!("/proc/sys/kernel/{}", param));
     }
 
     let _ = Command::new("sync").status();
@@ -439,6 +494,89 @@ pub fn initialize() {
     if !render.is_empty() && render != "Default" {
         let _ = Command::new("sys.azenith-utilityconf").args(["setrender", &render]).status();
     }
+    
+    // Set thermal governor to step_wise
+    if let Ok(paths) = glob::glob("/sys/class/thermal/thermal_zone*") {
+        for path in paths.flatten() {
+            if let Some(p_str) = path.to_str() {
+                zeshia_def("step_wise", &format!("{}/policy", p_str));
+            }
+        }
+    }
+    
+    // I/O Tweaks
+    if let Ok(paths) = glob::glob("/sys/block/*") {
+        for path in paths.flatten() {
+            if let Some(p_str) = path.to_str() {
+                zeshia_def("0", &format!("{}/queue/iostats", p_str));
+                zeshia_def("0", &format!("{}/queue/add_random", p_str));
+            }
+        }
+    }
+
+    // Networking tweaks
+    let tcp_avail = fs::read_to_string("/proc/sys/net/ipv4/tcp_available_congestion_control").unwrap_or_default();
+    let algos = ["bbr3", "bbr2", "bbrplus", "bbr", "westwood", "cubic"];
+    for algo in algos.iter() {
+        if tcp_avail.contains(algo) {
+            zeshia_def(algo, "/proc/sys/net/ipv4/tcp_congestion_control");
+            break;
+        }
+    }
+
+    zeshia_def("1", "/proc/sys/net/ipv4/tcp_low_latency");
+    zeshia_def("1", "/proc/sys/net/ipv4/tcp_ecn");
+    zeshia_def("3", "/proc/sys/net/ipv4/tcp_fastopen");
+    zeshia_def("1", "/proc/sys/net/ipv4/tcp_sack");
+    zeshia_def("0", "/proc/sys/net/ipv4/tcp_timestamps");
+
+    // Limit max perf event processing time to this much CPU usage
+    zeshia_def("3", "/proc/sys/kernel/perf_cpu_time_max_percent");
+
+    // Disable schedstats
+    zeshia_def("0", "/proc/sys/kernel/sched_schedstats");
+
+    // Disable Oppo/Realme cpustats
+    zeshia_def("0", "/proc/sys/kernel/task_cpustats_enable");
+
+    // Disable Sched auto group
+    zeshia_def("0", "/proc/sys/kernel/sched_autogroup_enabled");
+
+    // Enable CRF
+    zeshia_def("1", "/proc/sys/kernel/sched_child_runs_first");
+
+    // Improve real time latencies by reducing the scheduler migration time
+    zeshia_def("32", "/proc/sys/kernel/sched_nr_migrate");
+
+    // Tweaking scheduler to reduce latency
+    zeshia_def("50000", "/proc/sys/kernel/sched_migration_cost_ns");
+    zeshia_def("1000000", "/proc/sys/kernel/sched_min_granularity_ns");
+    zeshia_def("1500000", "/proc/sys/kernel/sched_wakeup_granularity_ns");
+
+    // Disable read-ahead for swap devices
+    zeshia_def("0", "/proc/sys/vm/page-cluster");
+
+    // Update /proc/stat less often to reduce jitter
+    zeshia_def("15", "/proc/sys/vm/stat_interval");
+
+    // Disable compaction_proactiveness
+    zeshia_def("0", "/proc/sys/vm/compaction_proactiveness");
+
+    // Disable SPI CRC
+    zeshia_def("0", "/sys/module/mmc_core/parameters/use_spi_crc");
+
+    // Disable OnePlus opchain
+    zeshia_def("0", "/sys/module/opchain/parameters/chain_on");
+
+    // Disable Oplus bloats
+    zeshia_def("0", "/sys/module/cpufreq_bouncing/parameters/enable");
+    zeshia_def("0", "/proc/task_info/task_sched_info/task_sched_info_enable");
+    zeshia_def("0", "/proc/oplus_scheduler/sched_assist/sched_assist_enabled");
+
+    // Report max CPU capabilities to these libraries
+    let libs = "libunity.so, libil2cpp.so, libmain.so, libUE4.so, libgodot_android.so, libgdx.so, libgdx-box2d.so, libminecraftpe.so, libLive2DCubismCore.so, libyuzu-android.so, libryujinx.so, libcitra-android.so, libhdr_pro_engine.so, libandroidx.graphics.path.so, libeffect.so";
+    zeshia_def(libs, "/proc/sys/kernel/sched_lib_name");
+    zeshia_def("255", "/proc/sys/kernel/sched_lib_mask_force");
 
     let _ = Command::new("sync").status();
     az_log("Initializing Complete");
